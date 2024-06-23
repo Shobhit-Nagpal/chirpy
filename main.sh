8664567cff4dfaa2d461ac37e9e65be9c1d83ff5
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-24 01:48:55 +0530

    change: shell script

--- main.sh
+++ main.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-# Build the Go project
-go build -o out
-
-# Get the name of the binary (assuming it's the same as the directory name)
-binary_name="out"
-
-# Check if 'bin' directory exists, create it if it doesn't
-if [ ! -d "bin" ]; then
-    mkdir bin
-fi
-
-# Move the binary to the 'bin' directory
-mv "out" bin/
-
-echo "Build complete. Binary moved to bin/out"
-
-./bin/out
+echo "Server restarted!"
+go run .
