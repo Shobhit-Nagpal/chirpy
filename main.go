f456c7518885b5e51122930ce63a2bc5bc65df92
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-24 09:28:15 +0530

    add: readiness handler + prefix changes

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
+
+func handleReadiness(w http.ResponseWriter, req *http.Request) {
+  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+  io.WriteString(w, "OK")
+  w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -16,8 +23,9 @@ func main() {
     return
   }
 
-  serveMux.Handle("/", http.FileServer(http.Dir(pwd)))
+  serveMux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(pwd))))
   serveMux.Handle("/assets", http.FileServer(http.Dir(fmt.Sprintf("%s/assets", pwd))))
+  serveMux.HandleFunc("/healthz", handleReadiness)
 
 	server := &http.Server{
 		Handler: serveMux,
